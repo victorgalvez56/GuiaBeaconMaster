8370235feb2cf167c5467dac6293c880fb2de287
victorgalvez56 <victor.galvez56@gmail.com>
2019-06-07 13:49:19 -0500

    Revert "Conexión a Beacon"
    This reverts commit d0d29bd02fec37c1491eaf1f9ad4dd157805f8f8.

--- app/src/main/java/com/example/guiabeaconmaster/Panel.java
+++ app/src/main/java/com/example/guiabeaconmaster/Panel.java
@@ -1,10 +1,22 @@
 package com.example.guiabeaconmaster;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,10 +26,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Panel extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    protected final String TAG = Panel.this.getClass().getSimpleName();;
 
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +41,14 @@ public class Panel extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -74,12 +97,29 @@ public class Panel extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        FragmentManager fm= getSupportFragmentManager();
+
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-            Intent intencion = new Intent(getApplicationContext(),Horarios2.class);
-            startActivity(intencion);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                // Si los permisos de localización todavía no se han concedido, solicitarlos
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    askForLocationPermissions();
+
+                } else { // Permisos de localización concedidos
+
+                    prepareDetection();
+                }
+
+            } else { // Versiones de Android < 6
+
+                prepareDetection();
+            }
+
+
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -88,5 +128,88 @@ public class Panel extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private void askForLocationPermissions() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.location_access_needed);
+        builder.setMessage(R.string.grant_location_access);
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onDismiss(DialogInterface dialog) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        });
+        builder.show();
+    }
+
+    private void prepareDetection() {
+
+        if (!isLocationEnabled()) {
+
+            askToTurnOnLocation();
+
+        } else { // Localización activada, comprobemos el bluetooth
+
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (mBluetoothAdapter == null) {
+
+                showToastMessage(getString(R.string.not_support_bluetooth_msg));
+
+            } else if (mBluetoothAdapter.isEnabled()) {
+                FragmentManager fm= getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.escenariopanel,new Horarios()).commit();
+
+            } else {
+
+                // Pedir al usuario que active el bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+            }
+        }
+    }
+
+    private boolean isLocationEnabled() {
+
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        boolean networkLocationEnabled = false;
+
+        boolean gpsLocationEnabled = false;
+
+        try {
+            networkLocationEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            gpsLocationEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        } catch (Exception ex) {
+            Log.d(TAG, "Excepción al obtener información de localización");
+        }
+
+        return networkLocationEnabled || gpsLocationEnabled;
+    }
+    private void askToTurnOnLocation() {
+
+        // Notificar al usuario
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(R.string.location_disabled);
+        dialog.setPositiveButton(R.string.location_settings, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                // TODO Auto-generated method stub
+                Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(myIntent);
+            }
+        });
+        dialog.show();
+    }
+
+    private void showToastMessage (String message) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
